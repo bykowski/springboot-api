5da85902065f004132cf6c3ebe5cefc7441e1e56
Przemyslaw Bykowski <p.bykowski@sdacademy.pl>
2019-05-26 10:32:04 +0200

    animal

--- src/main/java/pl/bykowski/springbootapi/MyApi.java
+++ src/main/java/pl/bykowski/springbootapi/MyApi.java
@@ -1,14 +1,29 @@
 package pl.bykowski.springbootapi;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MyApi {
 
     @GetMapping("/hello")
-    public String get() {
-        return "Hello";
+    public Animal get() {
+        Animal animal = new Animal();
+        animal.setName("Pucek");
+        animal.setAge(9);
+        return animal;
     }
 
+    @PostMapping("/hello")
+    public String post() {
+        return "post";
+    }
+
+    @DeleteMapping("/hello")
+    public String delete() {
+        return "delete";
+    }
 }
