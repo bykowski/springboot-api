3a9492616807a36261c08d74d91b1e1527e63c6a
Przemyslaw Bykowski <p.bykowski@sdacademy.pl>
2019-05-26 12:57:38 +0200

    db

--- src/main/java/pl/bykowski/springbootapi/Animal.java
+++ src/main/java/pl/bykowski/springbootapi/Animal.java
@@ -1,7 +1,15 @@
 package pl.bykowski.springbootapi;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Animal {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Animal(Long id, String name, int age) {
